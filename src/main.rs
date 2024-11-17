0a69c98b8804bab96b489c74ba05535278744c41
c483481 <73603369+c483481@users.noreply.github.com>
2024-11-17 23:08:42 +0800

    feat: add init app config from env

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,18 @@
 use actix_web::{middleware::Logger, App, HttpServer};
 use app_info::manifest_app;
+use config::config::Config;
 use env_logger::Env;
+use std::time::Instant;
+
 mod response;
 mod app_info;
-use std::time::Instant;
+mod config;
 
 use lazy_static::lazy_static;
 
 lazy_static! {
     static ref START_TIME: Instant = Instant::now();
+    static ref CONFIG: Config = Config::new();
 }
 
 const APP_NAME: &str = "codebase actix-web";
@@ -17,14 +21,14 @@ const APP_VERSION: &str = "1.0.0";
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     env_logger::init_from_env(Env::default().default_filter_or("info"));
-    println!("🚀 Server running at http://localhost:3000");
+    println!("🚀 Server running at http://localhost:{}", CONFIG.port);
 
     HttpServer::new(|| {
         App::new()
             .wrap(Logger::new("%a '%r' %s %b '%{Referer}i' %Dms"))
             .service(manifest_app)
     })
-    .bind(("127.0.0.1", 3000))?
+    .bind(("127.0.0.1", CONFIG.port))?
     .run()
     .await
 }
